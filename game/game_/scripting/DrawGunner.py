6e44e6a79c3a6becfaba3f313e6209385dadb1ac
caleb <caleb.malcarne@gmail.com>
2022-07-11 09:50:18 -0600

    drawGunner changes

--- game/game_/scripting/DrawGunner.py
+++ game/game_/scripting/DrawGunner.py
@@ -5,11 +5,24 @@ from game_.casting.rectangle import Rectangle
 from constants import *
 
 TEST_GROUP = "test"
+BLUE = 0
 
 class drawgunner(Action):
     def __init__(self, video_service):
         self._video_service = video_service
-        
+
+    def draw_Lines(self, C, body):
+        position = body.get_position()
+        line_start_x = position.get_x()
+        line_start_y = position.get_y()
+
+        line_end_x_a = 640
+        line_end_y_a = 480
+        line_end_x_b = 0
+        self._video_service.draw_line(line_start_x, line_start_y, line_end_x_a, line_end_y_a, C, C)
+        self._video_service.draw_line(line_start_x, line_start_y, line_end_x_b, line_end_y_a, C, C)
+
+
     def execute(self, cast, script, callback):
         testobj = cast.get_first_actor(TEST_GROUP)
         body = testobj.get_body()
@@ -20,14 +33,6 @@ class drawgunner(Action):
         
         image =  testobj.get_image()
         position = body.get_position()
-        line_start_x = position.get_x()
-        line_start_y = position.get_y()
-
-        line_end_x_a = 640
-        line_end_y_a = 480
-        line_end_x_b = 0
-        self._video_service.draw_line(line_start_x, line_start_y, line_end_x_a, line_end_y_a, [255,255,255,255], [255,255,255,255])
-        self._video_service.draw_line(line_start_x, line_start_y, line_end_x_b, line_end_y_a, [255,255,255,255], [255,255,255,255])
-        
+        self.draw_Lines((255,255,255,255), body)
         #self._video_service.draw_image(image, position)
             
