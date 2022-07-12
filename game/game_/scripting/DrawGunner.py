2eb8e1d03821e8319c08d3b7e196490bedc52eca
caleb <caleb.malcarne@gmail.com>
2022-07-12 09:46:34 -0600

    text changes

--- game/game_/scripting/DrawGunner.py
+++ game/game_/scripting/DrawGunner.py
@@ -13,11 +13,7 @@ class drawgunner(Action):
         self._mouse_service = mouse_service
         self._video_service = video_service
         self._line_color = WHITE
-        
-    def shoot(self):
-        self.set_Line_Color(SHOOT)
-        time.sleep(.5)
-        self.set_Line_Color(WHITE)
+    
 
     def draw_Lines(self, C, body):
         position = body.get_position()
@@ -35,10 +31,11 @@ class drawgunner(Action):
 
     def execute(self, cast, script, callback):
         gunner = cast.get_first_actor(GUNNER_GROUP)
-        hp_text = cast.get_first_actor(GUNNER_HP_GROUP)
-        body = gunner.get_body()
+        #hp_label = cast.get_first_actor(GUNNER_HP_GROUP)
+        #hp_text = hp_label.get_text()
+        #hp_text.set_value(gunner.get_health())
 
-        #hp_text
+        body = gunner.get_body()
 
         if  gunner.is_debug():
             rectangle = Rectangle(body.get_position(), body.get_size())
--- game/game_/scripting/draw_hp.py
+++ game/game_/scripting/draw_hp.py
@@ -8,7 +8,10 @@ class DrawHPAction(Action):
         self._video_service = video_service
         
     def execute(self, cast, script, callback):
+        gunner = cast.get_first_actor(GUNNER_GROUP)
         gunner_hp = cast.get_first_actor(GUNNER_HP_GROUP)
         text = gunner_hp.get_text()
+        health = gunner.get_health()
+        text.set_value(health)
         position = gunner_hp.get_position()
         self._video_service.draw_text(text, position)
