227c620a136ead83eee91dd71267960ad9ecf7d0
Logan <logan.hitchcock54@gmail.com>
2022-07-12 22:59:35 -0600

    finished draw_boss and think thats all we need for it

--- game/game_/scripting/control_boss.py
+++ game/game_/scripting/control_boss.py
@@ -2,9 +2,11 @@ from casting.boss import Boss
 #this deals with the movement of the boss, the volicity
 
 class ControlBoss():
-    boss = Boss()
 
     def move_boss(boss):
+        velocity = Boss.get_velocity()
+         
+
 
 
 
--- game/game_/scripting/draw_boss.py
+++ game/game_/scripting/draw_boss.py
@@ -1,4 +1,6 @@
 import random
+
+from pyray import draw_rectangle
 from casting.boss import Boss
 from game.constants import SCREEN_WIDTH
 #draws the boss and everything to do with the boss 
@@ -6,14 +8,13 @@ from game.constants import SCREEN_WIDTH
 
 class DrawBoss():
 
-    def __init__(self):
-        self.position_x = random.randint(SCREEN_WIDTH)
-        self.position_y = 0
-        self.velocity = 1
-
-
     def draw_body(self):
 
-        pass 
+        boss_width = 80
+        boss_height = 70
+        boss_x = Boss.get_x_location()
+        boss_y = Boss.get_y_location()
+        boss_color = Boss.boss_color()
 
 
+        draw_rectangle(boss_x, boss_y, boss_width, boss_height)
