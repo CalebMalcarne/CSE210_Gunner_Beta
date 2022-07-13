6df391eb0144f15a8350491cf84eb3b7af469597
mikedarling12 <mdarl1ng@byui.edu>
2022-07-13 09:37:16 -0600

    commit

--- game/game_/scripting/respawn_enemy.py
+++ game/game_/scripting/respawn_enemy.py
@@ -1,7 +1,29 @@
+import random
 from constants import *
+
 from game_.scripting.action import Action
+from game_.casting.enemy import Enemy
+from game_.casting.point import Point
+
+from game_.casting.body import Body
+from game_.casting.image import Image
 
 class Respawn_Enemy(Action):
 
-    def __init__(self, body, image):
-        super().__init__(body, image, False)
+    def __init__(self, cast):
+        super().__init__(False)
+        self.gunner = cast.get_first_actor(GUNNER_GROUP)
+        self.gunner_hits = self.gunner.get_health
+    
+    def spawn_enemy(amount_of_enemies):
+        for i in range(amount_of_enemies):
+            x = random.randint(1, 640)
+            y = 480
+            position = Point(x, y)
+            vx = 0
+            vy = 1
+            velocity = Point(vx,vy)
+            size = (10,10)
+            body = Body(position, size, velocity)
+            image = Image(TEST_IMAGE)
+            enemy = Enemy(body, image, False)
