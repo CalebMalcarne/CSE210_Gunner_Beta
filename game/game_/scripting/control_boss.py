75adcc32183c3ae8853edbc30b5e993f10bdf0c0
caleb <caleb.malcarne@gmail.com>
2022-07-13 01:50:28 -0600

    boss fix

--- game/game_/scripting/control_boss.py
+++ game/game_/scripting/control_boss.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-from casting.boss import Boss
-=======
 from game_.casting.boss import Boss
-#this deals with the movement of the boss, the volicity
->>>>>>> 8f0c2894f85075be6f0fe9e8a25b6dfe0627e601
 
 #this deals with the movement of the boss
 class ControlBoss():
