bc44713f028cb054fe449d9bb316eb74479f04ff
Caleb Malcarne <caleb.malcarne@gmail.com>
2022-07-14 12:01:45 -0600

    added background animation

--- game/game_/scripting/draw_stars_action.py
+++ game/game_/scripting/draw_stars_action.py
@@ -3,7 +3,13 @@ from game_.scripting.action import Action
 
 class DrawStars(Action):
     def __init__(self, video_service):
-        self.video_service = video_service
+        self._video_service = video_service
 
     def execute(self, cast, script, callback):
         stars = cast.get_first_actor(STAR_GROUP)
+        body = stars.get_body()
+        
+        animation = stars.get_animation()
+        image = animation.next_image()
+        position = body.get_position()
+        self._video_service.draw_image(image, position)
